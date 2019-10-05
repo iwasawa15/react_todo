697f213167f2af882d1b8bcdbb8099f85685412b
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-05 13:16:15 +0900

    addTodo methodの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
@@ -9,12 +9,22 @@ const App: React.FC = () => {
   const [tasks, setTasks] = useState([
     { name: "default", id: 0 },
   ]);
+  const [uniqueId, setId] = useState(0)
+
+  function addTodo(name: string){
+
+    tasks.push({
+      name,
+      id: uniqueId
+    })
+    setId(uniqueId + 1);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>TODO App</h2>
-        <TodoInput />
+        <TodoInput addTodo={ addTodo }/>
         <TodoList tasks = { tasks }/>
       </header>
     </div>
