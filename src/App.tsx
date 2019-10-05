c75e80effaaa2a4798a923e13d83d851b809fbac
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-05 11:58:51 +0900

    componentのimport

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
+import TodoItem from './TodoItem';
+import TodoInput from './TodoInput';
 
 const App: React.FC = () => {
+  const tasks = [
+    { name: "1", id: 0 },
+    { name: "2", id: 1 },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>TODO App</h2>
-        
+        <TodoInput />
+        <TodoList tasks = { tasks }/>
       </header>
     </div>
   );
