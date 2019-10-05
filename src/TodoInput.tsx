a06ed0833c3428b603e82bbedd5ba4b394cfb0ad
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-05 13:49:10 +0900

    入力の値をTodoに追加できる機能の実装

--- src/TodoInput.tsx
+++ src/TodoInput.tsx
@@ -1,11 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { render } from "react-dom";
+import { func } from "prop-types";
 
-const TodoInput: FC = () => {
+interface Props{
+	addTodo: (name: string) => void
+}
+
+const TodoInput: FC<Props> = (props) => {
+	const [inputValue, setValue] = useState('');
+
+	function handleClick(e: React.MouseEvent<HTMLButtonElement>){
+		e.preventDefault();
+		props.addTodo(inputValue)
+	}
+
+	function handleChange(e:React.ChangeEvent<HTMLInputElement>) {
+		e.preventDefault();
+		setValue(e.target.value);
+	}
 	return(
 		<div>
-			<input placeholder="新規TODOを入力してください" />
-			<button>登録</button>
+			<input placeholder="新規TODOを入力してください" value={inputValue} onChange={handleChange} />
+			<button onClick={handleClick}>登録</button>
 		</div>
 	);
 }
