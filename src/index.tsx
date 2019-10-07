ccf2352ff228f0f4fd418ca819bd11f85f4156bd
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-07 13:50:02 +0900

    Action initailStateの定義

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { Action } from 'redux'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+interface Task{
+	name: string
+}
+
+interface TasksState{
+	tasks: Task[]
+}
+
+const initialState: TasksState = {
+	tasks: []
+};
+
+interface TaskAction extends Action{
+	type: string,
+	payload: {
+		task: Task
+	}
+}
+
+function tasksReducer(state: TasksState = initialState, action: TaskAction){
+	switch(action.type){
+		case 'ADD_TASK':
+			return {
+				...state,
+				tasks: Object.assign(state.tasks, action.payload)
+			};
+		default:
+			return state;
+	}
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
