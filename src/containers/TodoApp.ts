4965d59834f9c93f8f29b50fc8ceaa25553d945a
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-27 02:56:14 +0900

    containerの定義

--- src/containers/TodoApp.ts
+++ src/containers/TodoApp.ts
@@ -1,6 +1,37 @@
-const mapStateToProps = ({ task, tasks }) => {
+import { addTask, inputTask } from "../actions/tasks";
+import { connect } from "react-redux";
+import TodoApp from "../components/TodoApp";
+import { Dispatch } from "redux";
+
+interface StateProps {
+  task: string;
+  tasks: string[];
+}
+
+interface DispatchProps {
+  addTask: (task: string) => void;
+  inputTask: (task: string) => void;
+}
+
+const mapStateToProps = (state: StateProps): StateProps => {
   return {
-    task,
-    tasks
+    task: state.task,
+    tasks: state.tasks
   };
 };
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return {
+    addTask(task) {
+      dispatch(addTask(task));
+    },
+    inputTask(task) {
+      dispatch(inputTask(task));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoApp);
